52cf75eaa0489fa9f7b7d82a96831ab8d0f93084
TadMat <tad@gifu-u.ac.jp>
2021-10-26 09:42:31 +0900

    fix: __init__

---
+++ swdata/__init__.py
@@ -0,0 +1,15 @@
+from .dataset import (
+    SWDataset,
+    LabelStruct,
+    Transcript,
+    sign2tensor,
+)
+
+__all__ = [
+    "SWDataset",
+    "LabelStruct",
+    "Transcript",
+    "sign2tensor",
+]
+
+__version__ = '0.1.0'
